8427b538eaff05cadd4a27cff34b55eea0231c12
Andry7Rz <andryhernandezpacheco@gmail.com>
2026-02-08 23:33:50 -0400

    mas cambios (toy cansado)

--- Scenes/dialogo_escena/balloon.gd
+++ Scenes/dialogo_escena/balloon.gd
@@ -1,50 +1,35 @@
 extends CanvasLayer
+
 ## A basic dialogue balloon for use with Dialogue Manager.
 
+# --- NUEVO: Nodo para el sonido de las letras ---
+# Asegúrate de crear un AudioStreamPlayer llamado "SonidoVoz" 
+# dentro de tu escena ExampleBalloon.
+@onready var sonido_voz: AudioStreamPlayer = $SonidoVoz 
 
 ## The dialogue resource
 @export var dialogue_resource: DialogueResource
 
-## Start from a given title when using balloon as a [Node] in a scene.
+## Resto de tus variables originales...
 @export var start_from_title: String = ""
-
-## If running as a [Node] in a scene then auto start the dialogue.
 @export var auto_start: bool = false
-
-## If all other input is blocked as long as dialogue is shown.
 @export var will_block_other_input: bool = true
-
-## The action to use for advancing the dialogue
 @export var next_action: StringName = &"ui_accept"
-
-## The action to use to skip typing the dialogue
 @export var skip_action: StringName = &"ui_cancel"
 
-## A sound player for voice lines (if they exist).
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
-
-## Temporary game states
 var temporary_game_states: Array = []
-
-## See if we are waiting for the player
 var is_waiting_for_input: bool = false
-
-## See if we are running a long mutation and should hide the balloon
 var will_hide_balloon: bool = false
-
-## A dictionary to store any ephemeral variables
 var locals: Dictionary = {}
-
 var _locale: String = TranslationServer.get_locale()
 
-## The current line
 var dialogue_line: DialogueLine:
 	set(value):
 		if value:
 			dialogue_line = value
 			apply_dialogue_line()
 		else:
-			# The dialogue has finished so close the balloon
 			if owner == null:
 				queue_free()
 			else:
@@ -52,30 +37,20 @@ var dialogue_line: DialogueLine:
 	get:
 		return dialogue_line
 
-## A cooldown timer for delaying the balloon hide when encountering a mutation.
 var mutation_cooldown: Timer = Timer.new()
-
-## The base balloon anchor
 @onready var balloon: Control = %Balloon
-
-## The label showing the name of the currently speaking character
 @onready var character_label: RichTextLabel = %CharacterLabel
-
-## The label showing the currently spoken dialogue
 @onready var dialogue_label: DialogueLabel = %DialogueLabel
-
-## The menu of responses
 @onready var responses_menu: DialogueResponsesMenu = %ResponsesMenu
-
-## Indicator to show that player can progress dialogue.
 @onready var progress: Polygon2D = %Progress
-
 
 func _ready() -> void:
 	balloon.hide()
 	Engine.get_singleton("DialogueManager").mutated.connect(_on_mutated)
 
-	# If the responses menu doesn't have a next action set, use this one
+	# --- NUEVO: Conectamos la señal de "hablar" del DialogueLabel ---
+	dialogue_label.spoke.connect(_on_dialogue_label_spoke)
+
 	if responses_menu.next_action.is_empty():
 		responses_menu.next_action = next_action
 
@@ -83,24 +58,29 @@ func _ready() -> void:
 	add_child(mutation_cooldown)
 
 	if auto_start:
-		if not is_instance_valid(dialogue_resource):
-			assert(false, DMConstants.get_error_message(DMConstants.ERR_MISSING_RESOURCE_FOR_AUTOSTART))
 		start()
 
+# --- NUEVA FUNCIÓN: El sonido tipo Undertale ---
+func _on_dialogue_label_spoke(letter: String, _letter_index: int, _speed: float) -> void:
+	# No suena en espacios ni si el sonido aún no existe
+	if letter == " " or letter == "" or sonido_voz == null:
+		return
+	
+	# Variamos el tono un poquito cada vez
+	sonido_voz.pitch_scale = randf_range(0.9, 1.1)
+	sonido_voz.play()
+
+## [EL RESTO DE TU CÓDIGO SE MANTIENE IGUAL ABAJO]
 
 func _process(delta: float) -> void:
 	if is_instance_valid(dialogue_line):
 		progress.visible = not dialogue_label.is_typing and dialogue_line.responses.size() == 0 and not dialogue_line.has_tag("voice")
 
-
 func _unhandled_input(_event: InputEvent) -> void:
-	# Only the balloon is allowed to handle input while it's showing
 	if will_block_other_input:
 		get_viewport().set_input_as_handled()
 
-
 func _notification(what: int) -> void:
-	## Detect a change of locale and update the current dialogue line to show the new language
 	if what == NOTIFICATION_TRANSLATION_CHANGED and _locale != TranslationServer.get_locale() and is_instance_valid(dialogue_label):
 		_locale = TranslationServer.get_locale()
 		var visible_ratio: float = dialogue_label.visible_ratio
@@ -108,8 +88,6 @@ func _notification(what: int) -> void:
 		if visible_ratio < 1:
 			dialogue_label.skip_typing()
 
-
-## Start some dialogue
 func start(with_dialogue_resource: DialogueResource = null, title: String = "", extra_game_states: Array = []) -> void:
 	temporary_game_states = [self] + extra_game_states
 	is_waiting_for_input = false
@@ -120,35 +98,24 @@ func start(with_dialogue_resource: DialogueResource = null, title: String = "", 
 	dialogue_line = await dialogue_resource.get_next_dialogue_line(start_from_title, temporary_game_states)
 	show()
 
-
-## Apply any changes to the balloon given a new [DialogueLine].
 func apply_dialogue_line() -> void:
 	mutation_cooldown.stop()
-
 	progress.hide()
 	is_waiting_for_input = false
 	balloon.focus_mode = Control.FOCUS_ALL
 	balloon.grab_focus()
-
 	character_label.visible = not dialogue_line.character.is_empty()
 	character_label.text = tr(dialogue_line.character, "dialogue")
-
 	dialogue_label.hide()
 	dialogue_label.dialogue_line = dialogue_line
-
 	responses_menu.hide()
 	responses_menu.responses = dialogue_line.responses
-
-	# Show our balloon
 	balloon.show()
 	will_hide_balloon = false
-
 	dialogue_label.show()
 	if not dialogue_line.text.is_empty():
 		dialogue_label.type_out()
 		await dialogue_label.finished_typing
-
-	# Wait for next line
 	if dialogue_line.has_tag("voice"):
 		audio_stream_player.stream = load(dialogue_line.get_tag_value("voice"))
 		audio_stream_player.play()
@@ -166,20 +133,13 @@ func apply_dialogue_line() -> void:
 		balloon.focus_mode = Control.FOCUS_ALL
 		balloon.grab_focus()
 
-
-## Go to the next line
 func next(next_id: String) -> void:
 	dialogue_line = await dialogue_resource.get_next_dialogue_line(next_id, temporary_game_states)
-
-
-#region Signals
-
 
 func _on_mutation_cooldown_timeout() -> void:
 	if will_hide_balloon:
 		will_hide_balloon = false
 		balloon.hide()
-
 
 func _on_mutated(_mutation: Dictionary) -> void:
 	if not _mutation.is_inline:
@@ -187,9 +147,7 @@ func _on_mutated(_mutation: Dictionary) -> void:
 		will_hide_balloon = true
 		mutation_cooldown.start(0.1)
 
-
 func _on_balloon_gui_input(event: InputEvent) -> void:
-	# See if we need to skip typing of the dialogue
 	if dialogue_label.is_typing:
 		var mouse_was_clicked: bool = event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()
 		var skip_button_was_pressed: bool = event.is_action_pressed(skip_action)
@@ -197,21 +155,13 @@ func _on_balloon_gui_input(event: InputEvent) -> void:
 			get_viewport().set_input_as_handled()
 			dialogue_label.skip_typing()
 			return
-
 	if not is_waiting_for_input: return
 	if dialogue_line.responses.size() > 0: return
-
-	# When there are no response options the balloon itself is the clickable thing
 	get_viewport().set_input_as_handled()
-
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 		next(dialogue_line.next_id)
 	elif event.is_action_pressed(next_action) and get_viewport().gui_get_focus_owner() == balloon:
 		next(dialogue_line.next_id)
 
-
 func _on_responses_menu_response_selected(response: DialogueResponse) -> void:
 	next(response.next_id)
-
-
-#endregion
--- assetss/SCRIPT_DE_DIALOGOS/PAPA.gd
+++ assetss/SCRIPT_DE_DIALOGOS/PAPA.gd
@@ -1,17 +1,62 @@
 extends Area2D
 
-# Esta línea permite que elijas la escena desde el Inspector haciendo clic
-@export_file("*.tscn") var escena_destino: String
+@export var exclamation_mark : CanvasItem 
+
+
+const TUTORIAL_PAPA = preload("uid://by7a7is87xdm6")
+
+
+var is_player_close = false
+var is_dialogo_active = false
+
+func _ready():
+	# Conexión de señales del DialogueManager
+	DialogueManager.dialogue_started.connect(_on_dialogo_started)
+	DialogueManager.dialogue_ended.connect(_on_dialogo_ended)
+	
+	
+	if not exclamation_mark:
+		exclamation_mark = get_node_or_null("ExclamationMark")
+	
+	if exclamation_mark:
+		exclamation_mark.visible = false
+		
+
+func _process(_delta):
+	# Cambiamos "abajo" por tu nueva acción "interactuar" (la tecla Z)
+	if is_player_close and Input.is_action_just_pressed("interactuar") and not is_dialogo_active:
+		DialogueManager.show_dialogue_balloon(TUTORIAL_PAPA)
 
 func _on_body_entered(body: Node2D) -> void:
-	# Esto nos dirá en consola TODO lo que pase por el área
-	print("ENTIDAD DETECTADA: ", body.name)
+	if body.is_in_group("jugador") or body is CharacterBody2D:
+		is_player_close = true
+		if exclamation_mark:
+			exclamation_mark.visible = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("jugador") or body is CharacterBody2D:
+		is_player_close = false
+		if exclamation_mark:
+			exclamation_mark.visible = false
+
+# --- ESTA PARTE BLOQUEA AL JUGADOR ---
+func _on_dialogo_started(_resource):
+	is_dialogo_active = true
+	if exclamation_mark:
+		exclamation_mark.visible = false
 	
-	# Intentamos detectar al jugador de tres formas diferentes para estar seguros
-	if body.is_in_group("jugador") or body is CharacterBody2D or "Player" in body.name:
-		print("¡JUGADOR CONFIRMADO! Iniciando cambio de escena...")
+	# Buscamos al jugador y le decimos que se detenga
+	var player = get_tree().get_first_node_in_group("jugador")
+	if player and player.has_method("set_hablando"):
+		player.set_hablando(true)
+
+func _on_dialogo_ended(_resource):
+	is_dialogo_active = false
+	
+	# Buscamos al jugador y le devolvemos el movimiento
+	var player = get_tree().get_first_node_in_group("jugador")
+	if player and player.has_method("set_hablando"):
+		player.set_hablando(false)
 		
-		if escena_destino != "":
-			get_tree().change_scene_to_file(escena_destino)
-		else:
-			print("ERROR: No arrastraste la escena del menú al Inspector del Area2D")
+	if is_player_close and exclamation_mark:
+		exclamation_mark.visible = true
